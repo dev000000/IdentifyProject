2d898e5d2f2f35173c1c1f5058dbf6e6e2e83ebe
DEV00000001 <vuongdachaivang@gmail.com>
2025-08-27 15:19:36 +0700

    #13-dev001-learn-method-security-with-pre-post-authorize

--- src/main/java/com/dev001/identify/service/UserService.java
+++ src/main/java/com/dev001/identify/service/UserService.java
@@ -16,6 +16,7 @@ public interface UserService {
     UserResponse getUserDetail(String id);
     UserResponse updateUser(String id, UserUpdateRequest request);
     void deleteUser(String id);
+    UserResponse getMyProfile();
 
 
 
