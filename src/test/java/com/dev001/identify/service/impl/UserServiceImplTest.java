6c4f7859374a5e26481b17a17818d598a9fa60c0
DEV00000001 <vuongdachaivang@gmail.com>
2025-09-02 17:32:36 +0700

    #31-dev001-add-getMyProfile-method-testing

--- src/test/java/com/dev001/identify/service/impl/UserServiceImplTest.java
+++ src/test/java/com/dev001/identify/service/impl/UserServiceImplTest.java
@@ -2,8 +2,10 @@ package com.dev001.identify.service.impl;
 
 import com.dev001.identify.dto.response.UserResponse;
 import com.dev001.identify.entity.user.User;
+import com.dev001.identify.exception.AppException;
 import com.dev001.identify.mapper.UserMapper;
 import com.dev001.identify.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -11,37 +13,44 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-
 class UserServiceImplTest {
 
-//  InjectMocks: create an actual instance of the class under test and automatically inject the mock dependencies into it.
+    //  InjectMocks: create an actual instance of the class under test and automatically inject the mock dependencies into it.
     @InjectMocks
     private UserServiceImpl underTest;
 
-//  Mock: create a mock instance of the class under test and inject it into the class under test.
-    @Mock private UserRepository userRepository;
-    @Mock private UserMapper userMapper;
+    //  Mock: create a mock instance of the class under test and inject it into the class under test.
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
 
 
     private User user;
     private UserResponse userResponse;
 
-//  BeforeEach: run before each test method.
+    //  BeforeEach: run before each test method.
     @BeforeEach
     void setUp() {
         user = User.builder()
                 .id("1")
-                .userName("test")
-                .passWord("")
+                .userName("usernameTest")
+                .passWord("ksdlfjsdlfk")
                 .firstName("test")
                 .lastName("test")
                 .dob(LocalDate.parse("2007-08-28"))
@@ -49,20 +58,31 @@ class UserServiceImplTest {
                 .build();
         userResponse = UserResponse.builder()
                 .id("1")
-                .userName("test")
-                .passWord("")
+                .userName("usernameTest")
+                .passWord("ksdlfjsdlfk")
                 .firstName("test")
                 .lastName("test")
                 .dob(LocalDate.parse("2007-08-28"))
                 .roles(new HashSet<>())
                 .build();
+
+        var auth = new UsernamePasswordAuthenticationToken(
+                "usernameTest",
+                "pwd",
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+        SecurityContextHolder.getContext().setAuthentication(auth);
     }
 
-//  @Test: marks a method as a unit test.
+    @AfterEach
+    void clearSecurityContext() {
+        SecurityContextHolder.clearContext();
+    }
+
+    //  @Test: marks a method as a unit test.
     @Test
 //  @Disabled: disable test method, this method will not be executed when you run the test class.
     @Disabled
-
     void createUser() {
     }
 
@@ -87,9 +107,33 @@ class UserServiceImplTest {
     void getUserDetail() {
     }
 
+
     @Test
-    @Disabled
-    void getMyProfile() {
+    @WithMockUser(username = "usernameTest")
+    void getMyProfile_validUser_shouldReturnProfile() {
+        //given
+        when(userRepository.findByUserName("usernameTest")).thenReturn(Optional.of(user));
+        when(userMapper.toUserResponse(user)).thenReturn(userResponse);
+        //when
+        UserResponse response = underTest.getMyProfile();
+        //then
+        assertThat(response.getUserName()).isEqualTo("usernameTest");
+        assertThat(response.getFirstName()).isEqualTo("test");
+        assertThat(response.getLastName()).isEqualTo("test");
+        verify(userMapper).toUserResponse(user);
+    }
+
+    @Test
+    void getMyProfile_invalidUser_shouldThrowException() {
+        //given
+        when(userRepository.findByUserName("usernameTest")).thenReturn(Optional.empty());
+        //when
+        assertThatThrownBy(() -> underTest.getMyProfile())
+                .isInstanceOf(AppException.class)
+                .hasFieldOrPropertyWithValue("errorCode.code",1002)
+                .hasFieldOrPropertyWithValue("errorCode.message","User not found");
+        //then
+        verify(userMapper,never()).toUserResponse(user);
     }
 
     @Test
