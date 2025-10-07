4d1294895156c26651c0bbc59e4cd12947ebe9e7
DEV00000001 <vuongdachaivang@gmail.com>
2025-10-07 22:29:30 +0700

    #62-dev001-refactor-file-structure-and-dynamic-env-setup

--- BACKEND/src/main/java/com/dev001/identify/IdentifyApplication.java
+++ BACKEND/src/main/java/com/dev001/identify/IdentifyApplication.java
@@ -2,17 +2,48 @@ package com.dev001.identify;
 
 import static com.dev001.identify.enums.Role.*;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
 public class IdentifyApplication {
+    @Value("${server.port}")
+    public  String port;
+
+    @Value("${application.security.cookie.secure}")
+    private  boolean secure;
+
+    @Value("${application.security.cookie.same-site}")
+    private  String sameSite;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private  long refreshExpiration; // in milliseconds
+
+    @Value("${spring.datasource.url}")
+    private  String url;
+
     public static void main(String[] args) {
         SpringApplication.run(IdentifyApplication.class, args);
+
     }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            log.error("Server started on port: " + port);
+            log.error("Secure: " + secure);
+            log.error("SameSite: " + sameSite);
+            log.error("Refresh Expiration: " + refreshExpiration);
+            log.error("Database URL: " + url);
+        };
+    }
+    }
+
 
     // this section for test token
     //    @Bean
@@ -47,4 +78,4 @@ public class IdentifyApplication {
     //            log.error("User token:" + authenticationService.register(user).getAccessToken());
     //        };
     //    }
-}
+
